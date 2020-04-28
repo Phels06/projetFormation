fd2a3c4fdd0db2f8c68e2a711968a44282d69d7e
phels06 <cedric.phelisse@protonmail.com>
2020-04-28 16:06:13 +0200

    _commit annonce note prix_

---
+++ src/main/java/projetFormation/entity/Note.java
@@ -0,0 +1,15 @@
+package projetFormation.entity;
+
+public enum Note {
+	N0("0"), N1("1"), N2("2"), N3("3"), N4("4"), N5("5");
+
+	private String label;
+
+	private Note(String label) {
+		this.label = label;
+	}
+
+	public String getNote() {
+		return label;
+	}
+}
