7d4c9a3c87f6b704a01575d4317d67dd3f4ff799
phels06 <cedric.phelisse@protonmail.com>
2020-05-05 17:08:35 +0200

    __

--- src/main/java/projetFormation/service/PostulerService.java
+++ src/main/java/projetFormation/service/PostulerService.java
@@ -5,8 +5,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import projetFormation.entity.Annonce;
-import projetFormation.entity.Personne;
 import projetFormation.entity.Postuler;
 import projetFormation.entity.PostulerKey;
 import projetFormation.repository.PostulerRepository;
