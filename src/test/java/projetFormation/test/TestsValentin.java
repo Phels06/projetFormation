309de277817b9b271de49ffc5ed265f21b683311
phels06 <cedric.phelisse@protonmail.com>
2020-05-14 15:20:00 +0200

    class tests

---
+++ src/test/java/projetFormation/test/TestsValentin.java
@@ -0,0 +1,11 @@
+package projetFormation.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestChien.class })
+public class TestsValentin {
+
+}
